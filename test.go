7d01cd37c71097f5bc14eb07bca96dc92088d8cb
jamesfgreene <jamesfgreene@gmail.com>
2023-09-28 14:24:46 -0700

    Create test.go

---
+++ test.go
@@ -0,0 +1,40 @@
+
+
+password :=nil
+password:= null
+password :=   "Null"
+password        :=          "easypasswordceq"
+password := 'nil'
+password := `nuLL`
+password        :=          easypasswordceq
+
+
+password :nil
+password: null
+password :   ""
+password        :          'easypasswordcolon'
+password : ''
+password : ``
+
+
+password ==nil
+password== null
+password ==   ""
+password        ==          `easypassword2eq`
+password == ''
+password == ``
+
+password =nil
+password= null
+password =   ""
+password        =          "easypassword1eq"
+password = ''
+password = ``
+
+
+password ===nil
+password=== null
+password ===   ""
+password        ===          "easypassword3eq"
+password === ''
+password === ``
